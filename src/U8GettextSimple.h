32d0a627daaa5ebacfe3cb351c8fc61852f21b24
Hong-She Liang <starofrainnight@gmail.com>
2015-10-18 13:32:52 +0800

    Added simple substitute for U8Gettext series functions

---
+++ src/U8GettextSimple.h
@@ -0,0 +1,16 @@
+/**
+ * @file U8GettextSimple.h
+ *
+ * @date 2015-10-18
+ * @author Hong-She Liang <starofrainnight@gmail.com>
+ */
+
+#ifndef __U8GETTEXTSIMPLE_H_INCLUDED_3DC4756E755911E5AA6EA088B4D1658C
+#define __U8GETTEXTSIMPLE_H_INCLUDED_3DC4756E755911E5AA6EA088B4D1658C
+
+#include <U8Gettext.h>
+
+#define _(str) U8Gettext((str))
+#define N_(str) (str)
+
+#endif /* __U8GETTEXTSIMPLE_H_INCLUDED_3DC4756E755911E5AA6EA088B4D1658C */
